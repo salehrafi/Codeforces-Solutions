0f578cd8cbbcd3fda6f2395ee4e75518696ca209
Saleh Rafi <117371849+salehrafi@users.noreply.github.com>
2023-08-26 01:55:04 +0600

    Update A. Amusing Joke.cpp

--- A. Amusing Joke.cpp
+++ A. Amusing Joke.cpp
@@ -1,31 +1,17 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-    string str1, str2, fstr;
-    int x = 0, cnt = 0, y = 0;
-    cin >> str1 >> str2 >> fstr;
-    for(int i=0;i<(str1.length() + str2.length()) ;i++)
-    {
-        if(str1.at(x) == fstr.at(i))
-        {
-            x++;
-            cnt++;
-        }
-        if(str2.at(y) == fstr.at(i))
-        {
-            x++;
-            cnt++;
-        }
-    }
-    if(cnt == fstr.length()){
-            cout << "YES";
-        }
-    else{
-        cout << "NO";
-    }
-
-
+    int cnt = 0;
+    string ch1, ch2, ch3, s;
+    cin >> ch1 >> ch2 >> ch3;
+    s = ch1 + ch2;
+    sort(ch3.begin(), ch3.end());
+    sort(s.begin(), s.end());
+    if(s == ch3)
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
     return 0;
 }
