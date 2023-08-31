a7c407323ed251721b9dd8eb263672a0afd18e94
Saleh Rafi <117371849+salehrafi@users.noreply.github.com>
2023-08-31 17:19:49 +0600

    Add files via upload

---
+++ A. Holiday Of Equality.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	int arr[n];
+	int cnt = 0;
+	for(int i=0;i<n;i++)
+	{
+		cin >> arr[i];
+	}
+	sort(arr, arr+n);
+	for(int i=0;i<n-1;i++){
+		cnt = cnt+ (arr[n-1] - arr[i]);
+	}
+	cout << cnt << endl;
+	return 0;
+}
