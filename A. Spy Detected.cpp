5b89be23d843bcbfd4640086d4330fbda05536d8
Saleh Rafi <117371849+salehrafi@users.noreply.github.com>
2023-09-18 23:46:20 +0600

    Add files via upload

---
+++ A. Spy Detected.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    int n;
+    cin >> n;
+    for(int i=0; i<n; i++)
+    {
+        int x;
+        cin >> x;
+        int arr[x];
+        for(int i=0; i<x; i++)
+        {
+            cin >> arr[i];
+        }
+        int cnt = arr[0];
+        int f = 1;
+        for(int i=1; i<=x-1; i++)
+        {
+            if(arr[i-1] == arr[i])
+            {
+                cnt = arr[i];
+                break;
+            }
+        }
+       // cout << "MAX : " << cnt;
+
+        for(int i=1; i<=x-1; i++)
+        {
+            if(cnt != arr[i])
+            {
+                f = i + 1;
+                break;
+            }
+        }
+        cout << f << endl;
+    }
+    return 0;
+}
